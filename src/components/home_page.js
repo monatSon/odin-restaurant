a976c3fdaaaf5f86c551ce2d1d37ec42dd225e2d
Ngoc Son Nguyen <sony.nguyen@externity.cz>
2024-12-25 16:47:38 +0100

    updates

---
+++ src/components/home_page.js
@@ -0,0 +1,6 @@
+const AppendHomePage = () => {
+  const Content = document.getElementById("Content");
+  return (Content.innerHTML = "<h1>Hello</h1>");
+};
+
+export { AppendHomePage };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 console.log("JS running!");
+import { AppendHomePage } from "./components/home_page";
+
+AppendHomePage();
